2a8ad5fd3dfafaa06eba62f9abddb7d236d81a65
Md. Tahmidur Rahman Tushar <trtushar35@gmail.com>
2025-02-25 15:20:04 +0600

    create project and intial commit

---
+++ database/migrations/2025_02_25_065227_create_personal_access_tokens_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('tokenable_type', 191);  // Set length of tokenable_type to 191
+            $table->foreignId('tokenable_id');
+            $table->string('name', 191);
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
+            
+            // Index the tokenable_type and tokenable_id columns
+            $table->index(['tokenable_type', 'tokenable_id']);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personal_access_tokens');
+    }
+};
