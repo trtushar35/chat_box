f90c90257c6d4a1d124949389ad1bed1f9a0eaec
Md. Tahmidur Rahman Tushar <trtushar35@gmail.com>
2025-03-20 16:12:03 +0600

    create chat box

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Test\TestController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -17,4 +18,6 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+
 });
+Route::resource('javascripttest', TestController::class);
